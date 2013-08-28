506c3fc581fea5283037dbeb7098462cfc9393a5
Mike Gardiner <conversationing@gmail.com>
2013-08-28 11:43:29 +0800

    Add the role selection page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Ffstrike::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   root 'home#index'
+
+  get 'choose_role' => 'home#choose_role'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
