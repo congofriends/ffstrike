6927474c41aac02b8e9dae59a64c02209ef46dc5
Mike Gardiner <conversationing@gmail.com>
2013-09-03 17:33:26 +0800

    By default, teams are inactive.

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,5 +7,7 @@ class Team
   field :about_private,    :type => String
   field :phone,            :type => String
 
+  field :active,           :type => Boolean, :default => false
+
   belongs_to :coordinator, :class_name => "User"
 end
