63e1fb6a116fa5eaefc3d47bf3b66fe1e5f8e679
Mike Gardiner <conversationing@gmail.com>
2013-09-04 15:32:58 +0800

    Remove database truncation, Heroku won't have access to that stage of the deploy pipeline.

--- config/initializers/truncate_data.rb
+++
@@ -1,7 +0,0 @@
-# Truncate data models, especially during development.
-
-if Ffstrike::Application.config.truncate_models
-  Ffstrike::Application.config.truncate_models.each do |model|
-    model.to_s.camelcase.constantize.delete_all
-  end
-end
