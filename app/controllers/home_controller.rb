11d0c7c85e14277dcfdf3e42690d8994b67f5058
Mike Gardiner <conversationing@gmail.com>
2013-09-04 16:06:12 +0800

    Use devise user_signed_in?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def start
-    unless current_user.nil?
+    if user_signed_in?
       current_team = Team.any_in(:'coordinator.user_id' => current_user.id)
       if current_team.exists?
         redirect_to team_url(:id => current_team.first.id)
