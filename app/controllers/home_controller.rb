7c5a59f20d59e51f9e72951b39a06173825bf807
Mike Gardiner <conversationing@gmail.com>
2013-09-04 15:45:41 +0800

    Handle users who aren't logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
   def start
-    current_team = Team.any_in(:'coordinator.user_id' => current_user.id)
-    if current_team.exists?
-      redirect_to team_url(:id => current_team.first.id)
+    unless current_user.nil?
+      current_team = Team.any_in(:'coordinator.user_id' => current_user.id)
+      if current_team.exists?
+        redirect_to team_url(:id => current_team.first.id)
+      end
     end
   end
 end
