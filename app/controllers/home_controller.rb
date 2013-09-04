294b786feba907f4c4d33da8475392e2a000a369
Mike Gardiner <conversationing@gmail.com>
2013-09-04 10:02:13 +0800

    Go straight to your co-ordinator dashboard if you're co-ordinating an active team.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def start
+    current_team = Team.where(:coordinator => current_user, :active => true)
+    if current_team.exists?
+      redirect_to team_url(:id => current_team.first.id)
+    end
   end
 end
