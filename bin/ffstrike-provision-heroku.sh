0b85c4b833b379c2bb34c3708923dcddf20266fd
Mike Gardiner <conversationing@gmail.com>
2013-09-05 09:57:17 +0800

    Add a script to provision our heroku setup.

---
+++ bin/ffstrike-provision-heroku.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ffstrike-provision-heroku.sh APP_NAME"
+  exit 1
+fi
+
+APP_NAME=$1
+
+heroku apps:create --addons mongohq:sandbox $APP_NAME
+git push heroku master
+heroku ps:scale web=1 -a $APP_NAME
+
+echo "Remember: You'll need a corresponding Facebook app to refer to "
+echo "    http://$APP_NAME.herokuapp.com"
