8b3a86ebc89dff699472f4048b3b4ed78af56225
Mike Gardiner <conversationing@gmail.com>
2013-09-12 11:28:43 +0800

    Update a task count when tasks are checked off.

--- app/assets/javascripts/coordinator_dashboard.js
+++ app/assets/javascripts/coordinator_dashboard.js
@@ -1,10 +1,16 @@
 $(function() {
   $(".mark-task").click(function() {
-    var data = { done: $(this).is(':checked') };
+    var checked = $(this).is(':checked');
+
+    var data = { done: checked };
     $.ajax({
       data: data,
       url: '/tasks/' + $(this).data('team-id') + '/' + $(this).data('role-name') + '/' + $(this).data('task-id'),
       method: 'POST',
     });
+
+    var tasksLeftSpan = $(this).closest('.task-list').find('#tasks-to-do');
+    var currentCount = parseInt(tasksLeftSpan.text());
+    tasksLeftSpan.text(currentCount - (checked ? 1 : -1));
   });
 });
