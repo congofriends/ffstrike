1468541957f912c19118c4a6046ddb059a940dc2
Tim Minkov <timothyminkov@gmail.com>
2013-10-21 17:02:36 -0500

    Tim, non-logged in users can see teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index]
 
   def index
     unless params[:zip] and params[:distance]
