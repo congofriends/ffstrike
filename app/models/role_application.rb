dd45a20940743c755cabc6f9a28783a473b35bb0
Tim Minkov <timothyminkov@gmail.com>
2013-10-24 16:19:53 -0500

    Tim, fixed bug with applying with role

--- app/models/role_application.rb
+++ app/models/role_application.rb
@@ -15,5 +15,5 @@ class RoleApplication
   validates_format_of :phone, :with => /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
     :on => :create, :allow_blank => true, :message => "You must enter a valid phone number. Valid phone numbers are 10 digits long."
 
-  validates_presence_of :why_cause, :other, :message => "It is important for your team coordinator to know why you are interested... Please fill in the questions below."
+  validates_presence_of :why_cause, :other, :message => "It is important for your team coordinator to know why you are interested... Please fill in the questions below.", :on => :create
 end
