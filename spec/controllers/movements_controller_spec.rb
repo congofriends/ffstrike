13ebf116fde3b5b007abe7fdfb4e3c9491990f9f
Tim Minkov <timothyminkov@gmail.com>
2013-12-12 12:58:00 -0600

    fix tests to use factorygirl

--- spec/controllers/movements_controller_spec.rb
+++ spec/controllers/movements_controller_spec.rb
@@ -10,24 +10,19 @@ describe MovementsController do
   end
 
   describe "POST #create" do
-    before :each do
-      @movement = { :movement => {name: 'My Little Pony'} }
-      @invalid_movement = { :movement => {name: nil} }
-    end
-    
     it "responds successfully" do
-      post :create, @invalid_movement
+      post :create, movement: FactoryGirl.attributes_for(:nameless_movement)
 
       expect(response).to be_success 
     end
 
     context "with valid attributes" do
       it "creates a movement" do
-        expect{ post :create, @movement }.to change(Movement, :count).by(1) 
+        expect{ post :create, movement: FactoryGirl.attributes_for(:movement) }.to change(Movement, :count).by(1) 
       end
 
       it "redirects to the movement page" do
-        post :create, @movement 
+        post :create, movement: FactoryGirl.attributes_for(:movement)
 
         expect(response).to redirect_to movement_path(Movement.last)
       end
@@ -35,11 +30,11 @@ describe MovementsController do
 
     context "without valid name" do
       it "does not save the movement" do
-        expect{ post :create, @invalid_movement }.to_not change(Movement, :count) 
+        expect{ post :create, movement: FactoryGirl.attributes_for(:nameless_movement) }.to_not change(Movement, :count) 
       end
 
       it "re-renders the new method" do
-        post :create, @invalid_movement
+        post :create, movement: FactoryGirl.attributes_for(:nameless_movement) 
 
         expect(response).to render_template :new 
       end
