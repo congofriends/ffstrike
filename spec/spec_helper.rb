d70ef848e55ded62feefad386dd81ee475fc5ce8
loos <louisknapp@gmail.com>
2013-12-13 09:50:39 -0600

    add paperclip matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'simplecov'
+require 'paperclip/matchers'
 
 # Code coverage runs when you run rspec. This NEEDS to be at the top.
 SimpleCov.start
@@ -43,4 +44,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+end
+
+RSpec.configure do |config|
+  config.include Paperclip::Shoulda::Matchers
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
+require "paperclip/matchers"
+	
 class ActiveSupport::TestCase
+	extend Paperclip::Shoulda::Matchers
+
   ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
