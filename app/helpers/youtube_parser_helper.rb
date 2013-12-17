c60ca12a84513e5ff89477d42712249e105ae894
Svetlana Ivanova <sivanova@thoughtworks.com>
2013-12-17 14:06:32 -0600

    extract building embed youtube link to youtube_parser_helper

--- app/helpers/youtube_parser_helper.rb
+++ app/helpers/youtube_parser_helper.rb
@@ -6,8 +6,10 @@ module YoutubeParserHelper
     if match = REGULAR.match(youtube_url)
       return match[4]
     end
-
     nil
   end
 
+  def youtube_embed_url_for_(youtube_id)
+    %(<iframe src="http://www.youtube.com/embed/#{youtube_id}" frameborder="0" allowfullscreen></iframe>).html_safe
+  end
 end   
