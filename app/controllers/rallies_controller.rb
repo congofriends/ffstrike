5b848f46f33d095178c0ccfbe5a8747010efd4dd
Svetlana Ivanova <sivanova@thoughtworks.com>
2013-12-19 11:30:41 -0600

    improvement: show users real error text message instead of hardcoded one

--- app/controllers/rallies_controller.rb
+++ app/controllers/rallies_controller.rb
@@ -11,7 +11,7 @@ class RalliesController < ApplicationController
       flash[:notice] = "Rally created!"
       redirect_to movement_path(@movement)
     else
-      flash[:notice] = "Rally was not created."
+      flash[:notice] = @rally.errors.full_messages.flatten.join
       render :new
     end
   end
