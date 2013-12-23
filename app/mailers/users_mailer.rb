5d76beb927a4d96488eb702cd35c8552d0e24a0c
loos <louisknapp@gmail.com>
2013-12-23 12:21:47 -0600

    add email_all_attendees

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -2,14 +2,8 @@ class UsersMailer < ActionMailer::Base
   default from: "coordinator@rallies.com"
 
   def custom_message_all_attendees(movement, message)
-    Attendee.all.where(movement_id: movement.id).each do |attendee|
-      send_custom_message(attendee.email, message)
-    end 
+    @recipients = Attendee.all.where(movement_id: movement.id) 
+    @message = message
+    mail(:to => @recipients.collect(&:email).join(","))
   end
-
-  def send_custom_message(email, message)
-    @message = message 
-    mail to: email
-  end
-
 end
--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -2,11 +2,14 @@ require "spec_helper"
 
 describe UsersMailer do
   describe "custom_message_all_attendees" do
+
     let(:mail) { UsersMailer.custom_message_all_attendees(@movement, "Test Email Message") }
+
     before :each do
-      @user = FactoryGirl.create(:user)
-      @attendee = FactoryGirl.create(:attendee)
       @movement = FactoryGirl.create(:movement) 
+      @movement_resource = {movement_id: @movement.id}
+      @attendee = FactoryGirl.create(:attendee, @movement_resource)
+      @user = FactoryGirl.create(:user, @movement_resource)
     end
                                 
     it "renders the headers" do
@@ -18,6 +21,15 @@ describe UsersMailer do
     it "renders the body" do
       mail.body.encoded.should match("Test Email Message")
     end
+
+    it "mails all attendees" do
+      another_attendee = FactoryGirl.create :attendee, email: "TestAllAttendees@example.com", movement_id: @movement.id 
+      mail.to.should include("TestAllAttendees@example.com")
+    end
+
+    it "doesnt mail other movements" do 
+    end
+
   end
 
 end
