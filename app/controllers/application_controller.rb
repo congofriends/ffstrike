8401018a613cc0c1d43517b471489229bb8f41a5
loos <louisknapp@gmail.com>
2013-12-23 10:06:59 -0600

    configure to redirect to user's movement on signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #def after_sign_in_path(resource)
-  #  movement_path(current_user.movement_id)
-  #end
+  def after_sign_in_path_for(resource)
+    movement_path(current_user.movement_id)
+  end
 end
