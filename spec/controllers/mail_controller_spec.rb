14387b23f2048b2a82d3310b8cf0a2496a16cc14
loos <louisknapp@gmail.com>
2014-01-07 15:23:42 -0600

    refactor mail_controller_spec

--- spec/controllers/mail_controller_spec.rb
+++ spec/controllers/mail_controller_spec.rb
@@ -2,29 +2,21 @@ require 'spec_helper'
 
 describe MailController do
 
+  before :each do
+    attendee = FactoryGirl.create(:attendee, movement_id: "1", rally_id: "1")
+  end
+
   describe "POST #mail" do
-
-    before :each do
-      attendee = FactoryGirl.create(:attendee, movement_id: "1")
-    end
-
     it "tells usermailer to mail" do
       post "mail_movement", movement_id: "1", message: "TestMessage"
       assert !ActionMailer::Base.deliveries.empty?
     end
-
   end
 
   describe "POST #mail_rally" do
-
-    before :each do
-      attendee = FactoryGirl.create(:attendee, movement_id: "1", rally_id: "1")
-    end
-
     it "tells usermailer to mail_rally" do
       post "mail_rally", rally_id: "1", movement_id: "1", message: "Test Message"
       assert !ActionMailer::Base.deliveries.empty?
     end
-
   end 
 end
