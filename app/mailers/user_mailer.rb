f687b507e67e6d7d241f25dc4a5ac58ecf493dbb
loos <louisknapp@gmail.com>
2014-01-07 13:59:20 -0600

    add mail individual rallies backend

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,11 @@ class UserMailer < ActionMailer::Base
     end
     return false
   end
+
+  def custom_message_rally(rally_id, message)
+    @recipients = Attendee.all.where(rally_id: rally_id)
+    @message = message
+    mail(to: @recipients.collect(&:email).join(","))
+  end
+
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,7 +4,6 @@ describe UserMailer do
   describe "custom_message_all_attendees" do
 
     let(:mail) { UserMailer.custom_message_all_attendees(@movement, "Test Email Message") }
-
     let(:another_user) { FactoryGirl.create(:user, email: "another_user@example.com") }
     let(:another_rally) { FactoryGirl.create(:rally, coordinator: another_user) }
 
@@ -16,7 +15,7 @@ describe UserMailer do
     end
                                 
     it "renders the headers" do
-      mail.subject.should eq("Custom message from your Movement Organizer")
+      mail.subject.should eq("Message from your Movement Organizer")
       mail.to.should eq(["email@example.com"])
       mail.from.should eq(["coordinator@rallies.com"])
     end
@@ -25,7 +24,7 @@ describe UserMailer do
       mail.body.encoded.should match("Test Email Message")
     end
 
-    it "mails all attendees" do
+    it "mails multiple attendees" do
       another_attendee = FactoryGirl.create :attendee, email: "TestAllAttendees@example.com", movement_id: @movement.id, rally: another_rally
       mail.to.should include("TestAllAttendees@example.com")
     end
@@ -33,6 +32,38 @@ describe UserMailer do
     it "doesnt mail other movements" do 
       different_movement_attendee = FactoryGirl.create :attendee, email: "AnotherMovement@example.com", movement_id: @movement.id + 1, rally: another_rally
       mail.to.should_not include("AnotherMovement@example.com")
+    end
+
+  end
+
+  describe "custom_message_rally" do
+
+    let(:mail) { UserMailer.custom_message_rally(@rally, "Test custom_message_rally") }
+    let(:another_user) { FactoryGirl.create(:user, email: "another_user@example.com") }
+    let(:another_rally) { FactoryGirl.create(:rally, coordinator: another_user) }
+
+    before :each do
+      @movement = FactoryGirl.create(:movement) 
+      @user = FactoryGirl.create(:user, movement_id: @movement.id)
+      @rally = FactoryGirl.create(:rally, movement_id: @movement.id, coordinator: @user)
+      @attendee = FactoryGirl.create(:attendee, movement_id: @movement.id, rally: @rally)
+    end
+     
+    it "renders the headers" do
+      mail.subject.should eq("Message from your Movement Organizer")
+      mail.to.should eq(["email@example.com"])
+      mail.from.should eq(["coordinator@rallies.com"])
+    end
+    it "renders the body" do
+      mail.body.encoded.should match("Test custom_message_rally")
+    end
+    it "mails multiple attendees in rally" do
+      another_attendee = FactoryGirl.create :attendee, email: "TestMultipleAttendees@example.com", movement_id: @movement.id, rally: @rally
+      mail.to.should include("TestMultipleAttendees@example.com")
+    end
+    it "doesn't mail attendee in other rally" do
+      other_rally_attendee = FactoryGirl.create :attendee, email: "OtherRally@example.com", movement_id: @movement.id, rally: another_rally
+      mail.to.should_not include("OtherRally@example.com")
     end
 
   end
