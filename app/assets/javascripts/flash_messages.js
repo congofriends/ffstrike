998409a70435c2e661ac0fa76d0b7efb42942441
Tim Minkov <timothyminkov@gmail.com>
2014-01-08 13:13:08 -0600

    made flash messages fade out and styled them a bit better

--- app/assets/javascripts/flash_messages.js
+++ app/assets/javascripts/flash_messages.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-   $('.flash').delay(2500).fadeOut(1000);
+   $('.flash').css('position', 'absolute');
+   $('.flash').css('left', 200);
+   $('.flash').css('right', 200);
+   $('.flash').css('margin-left', 'auto');
+   $('.flash').css('margin-right', 'auto');
+   $('.flash').css('z-index', '9999');
+   $('.flash').delay(1300).fadeOut();
 });
