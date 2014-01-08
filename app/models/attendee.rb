7cb9f977de77b36c0d7a65d60a0e5817f186b4fc
Svetlana Ivanova <sivanova@thoughtworks.com>
2014-01-08 11:23:06 -0600

    add model assosiation between attendee and movement; update rake task to create user with a password

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,5 +1,6 @@
 class Attendee < ActiveRecord::Base
   belongs_to :rally, class_name: Rally
+  belongs_to :movement
   validates :email, presence: true
   validates :rally_id, presence: true
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -3,6 +3,7 @@ class Movement < ActiveRecord::Base
   validates_presence_of :name, on: :create 
   has_many :tasks, dependent: :destroy
   has_many :rallies, dependent: :destroy
+  has_many :attendees
 
   has_attached_file :image, :default_url => 'cat.png'
 	validates_attachment_content_type :image, content_type: ['image/png', 'image/gif', 'image/jpg', 'image/jpeg']
