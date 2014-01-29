17115e45889071119542bdd559fd878fb041f1be
loos <louisknapp@gmail.com>
2014-01-29 15:15:45 -0600

    refactor task populator and fix tasks tab

--- app/models/task_populator.rb
+++ app/models/task_populator.rb
@@ -3,16 +3,18 @@ class TaskPopulator
   SPEAKOUT_TASKS = DEFAULT_EVENTS["speakout"]
   RALLY_TASKS = DEFAULT_EVENTS["rally"]
   
-  EVENT_TYPES = [["Speak Out", "Speak Out"],["Rally", "Rally"]]
+  EVENT_TYPE_FORM = [["Speak Out", "Speak Out"], ["Rally", "Rally"]] 
 
   def self.assign_tasks(event)
-    if event.event_type == "Speak Out"
-      SPEAKOUT_TASKS.each do |task_attributes|
-        event.tasks << Task.create(task_attributes)
-      end
-    end
-    if event.event_type == "Rally"
-      RALLY_TASKS.each do |task_attributes|
+    populate(event, "Speak Out", SPEAKOUT_TASKS)
+    populate(event, "Rally", RALLY_TASKS)
+  end
+
+  private
+
+  def self.populate(event, event_type, event_tasks)
+    if event.event_type == event_type
+      event_tasks.each do |task_attributes|
         event.tasks << Task.create(task_attributes)
       end
     end
