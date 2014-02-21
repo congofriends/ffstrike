e44bda8a6c8f342d40d91856a0a0347188102c67
Svetlana Ivanova <sivanova@thoughtworks.com>
2014-02-21 11:53:43 -0600

    fix tests in task_populator_spec

--- spec/models/task_populator_spec.rb
+++ spec/models/task_populator_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe TaskPopulator do
 
   it "assigns tasks to a speakout" do
-    speakout = FactoryGirl.build(:event, event_type: "Speak Out")
-    TaskPopulator.assign_tasks(speakout)
-    expect(speakout.tasks).not_to be_empty
+    speakout_event = FactoryGirl.build(:event, event_type_id: 2 )
+    TaskPopulator.assign_tasks(speakout_event)
+    expect(speakout_event.tasks).not_to be_empty
   end
 
   it "does not assign tasks to a custom event" do
-    popcorn_eating_contest = FactoryGirl.build(:event, event_type: "Popcorn Eating Contest")
-    TaskPopulator.assign_tasks(popcorn_eating_contest)
-    expect(popcorn_eating_contest.tasks).to be_empty
+    own_type_event = FactoryGirl.build(:event, event_type_id: 6)
+    TaskPopulator.assign_tasks(own_type_event)
+    expect(own_type_event.tasks).to be_empty
   end
 
 end
