ff1feb6d27db1919d580af9c4d325ba296780662
loos <louisknapp@gmail.com>
2014-02-27 11:29:49 -0600

    fix zipcode spec

--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Zipcode do
   context "#find_by_zip" do
     it "finds a zipcode" do
-      zip = FactoryGirl.create(:zipcode, zip: "60647")
-      expect(Zipcode.find_by_zip("60647")).to eq(zip)
+      zip = FactoryGirl.create(:zipcode, zip: "99999")
+      expect(Zipcode.find_by_zip("99999")).to eq(zip)
     end
   end
 end
