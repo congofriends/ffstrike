8ac8c2a2263057853549b14ca63e4b83d7b433da
loos <louisknapp@gmail.com>
2014-02-27 10:59:08 -0600

    add load zipcodes starting with 6 to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 types = %w(Rally Public\ speak\ out Movie\ screening Public\ forum Meet\ with\ decision\ maker Planning\ meeting Own\ event)
 types.each { |type| EventType.create(name: type)  }
 
+# Load Zipcodes beginning with 6
+file = "lib/assets/US.txt"
+puts "loading zip codes from #{file}"
+File.foreach(file) do |line|
+  data = line.split("\t")
+  if data[1][0] == "6"
+    Zipcode.create(zip: data[1], city: data[2], state: data[3], state_abbreviation: data[4], latitude: data[10].to_f, longitude: data[9].to_f)
+  end
+end
+
