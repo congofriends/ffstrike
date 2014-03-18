e2cd8ceed5b323802bb0b09100bdb4a59c3ecf48
Ehi Aimiuwu <eaimiuwu@icstars.org>
2014-03-18 16:50:28 -0500

    Test for creating an event

---
+++ features/coordinator_creates_event_for_a_movement.feature
@@ -0,0 +1,12 @@
+Feature: Creating an event as a Coordinator with an existing movement 
+
+Background:
+  Given a user account exists
+  Given I am logged in as a user
+  Given I am on the home page
+  Given I have an existing movement
+
+ Scenario:
+ When I select the Rally button
+ And I create an event
+ Then I can see the confirmation page
--- features/step_definitions/movement_creation.steps.rb
+++ features/step_definitions/movement_creation.steps.rb
@@ -10,12 +10,38 @@ Given(/^I am logged in as a user/) do
   click_link_or_button 'signin-button'
 end
 
+Given(/^I have an existing movement/) do 
+  click_link_or_button 'create-movement-link'
+  fill_in 'movement_name', with: 'Cats Move'
+  fill_in 'movement_tagline', with: 'Move yo cat!'
+  fill_in 'movement_call_to_action', with: 'Call yo cat'
+  fill_in 'movement_extended_description', with: 'extend yo cat'
+  fill_in 'movement_video', with: 'https://www.youtube.com/watch?v=Kdgt1ZHkvnM'
+  click_link_or_button 'create_movement'
+end
+
+When(/^I select the Rally button/)do
+  click_link_or_button 'rally'
+end
+
+When(/^I create an event/)do
+  fill_in 'event_name', with: 'Cats and Dogs'
+  fill_in 'event_address', with: '2373'
+  fill_in 'event_city', with: 'Chicago'
+  fill_in 'event_zip', with: '60649'
+  click_link_or_button 'create_movement'
+end
+
 Given(/^I am on the home page/) do
   visit root_path
 end
 
 When(/^I try to create a movement/) do
   click_link_or_button 'create-movement-link'
+end
+
+Then(/^I can see the confirmation page/)do
+  page.should have_text("successfully created a Rally")
 end
 
 And(/^I submit all the information that I can/) do
