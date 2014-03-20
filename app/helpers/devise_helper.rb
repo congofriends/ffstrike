92ed323452ee98368625ebe3bb88bd3d1abadacd
loos <louisknapp@gmail.com>
2014-03-20 14:21:53 -0500

    fix error messages

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,12 +1,17 @@
 module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
-    sentence = I18n.t("errors.messages.not_found")
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved")
+
     html = <<-HTML
     <div id="error_explanation">
-      <h3>#{sentence}</h3>
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
     </div>
     HTML
-    html.html_safe
+
+    html.html_safe  
   end
 end
