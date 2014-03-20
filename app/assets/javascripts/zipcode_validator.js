04eb63c9375bd7ad65e6b9fe3d5e29a4a4bfc2ea
ParkDan <dpinch86@gmail.com>
2014-03-20 15:25:27 -0500

    only allows zipcodes with 5 digits

--- app/assets/javascripts/zipcode_validator.js
+++ app/assets/javascripts/zipcode_validator.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
   $.validator.addMethod("zipDigitsDashSpaceOnlyRegex", function(value, element) {
-          return this.optional(element ) || /^ *\d{5}((-|\s)?\d{4})? *$/.test(value);
+          return this.optional(element ) || /^\d{5}$/.test(value);
   });
 
   $('#zip-form').validate({
     rules: {
       'zip': {
         required: true,
-        minlength: 5,
-        zipDigitsDashSpaceOnlyRegex: true
+        zipDigitsDashSpaceOnlyRegex: true,
       }
     },
     messages: {
       'zip': {
         required: "Zip code is required",
-        minlength: "Zip code must have at least five digits",
-        zipDigitsDashSpaceOnlyRegex: "Zip code should contain only digits, space or dash"
+        zipDigitsDashSpaceOnlyRegex: "Zip code should contain only digits",
       }
     },
     errorElement: "div",
