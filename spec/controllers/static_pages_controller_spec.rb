691b69b6d9bcb551c821e764e5fd7d99880fee9f
Svetlana Ivanova <sivanova@thoughtworks.com>
2014-03-21 09:50:01 -0500

    fix to the unit test: moving stuff to the before block was a bad idea

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe StaticPagesController do
   describe "GET #index" do
-    before(:each) { get 'index' }
 
     it "returns http success" do
+      get 'index'
       expect(response).to be_success
     end
 
     it "assigns 3 movements to @featured_movements" do
       FactoryGirl.create_list(:movement, 3)
+      get 'index'
       expect(assigns(:featured_movements).count).to be(3)
     end
   end
