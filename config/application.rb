270af8402eb5f5618f07c3481fa11324dbbbaa59
loos <louisknapp@gmail.com>
2014-04-02 14:14:53 -0500

    revert 9c6b

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,5 @@ module Ffstrike
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
-    config.assets.precompile += %w( *.css *.js *.coffee *.scss, *.css.scss.erb)
-    config.serve_static_assets = true
   end
 end
