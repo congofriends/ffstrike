5c3015f381fceac00db1c10346fa40caa18c938c
Ehi Aimiuwu <eaimiuwu@icstars.org>
2014-04-09 09:39:49 -0500

    adding the correct files and deleting the deletion feature

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -21,4 +21,20 @@ include Capybara::DSL
 		fill_in 'zip', with: ''
   		click_link_or_button 'search_zip'
 	end
+
+	def sign_in
+		click_link_or_button'Sign In'
+	end
+
+	def sign_up
+		click_link_or_button'Sign up'
+	end
+
+	def fill_in_sign_up_form
+		fill_in 'user_name', with: 'Jessica'
+  		fill_in 'user_email', with: 'jess@gmail.com'
+    	fill_in 'user_password', with: 'abc1234'
+    	fill_in 'user_password_confirmation', with: 'abc1234'
+    	click_link_or_button 'sign_up_button'
+	end
 end
