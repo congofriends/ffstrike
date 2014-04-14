903e7a66fa961877f6f6a647eb8a724060b86fe5
loos <louisknapp@gmail.com>
2014-04-14 14:43:30 -0500

    invite works for user with one movement

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,15 +1,17 @@
 class Users::InvitationsController < Devise::InvitationsController
   def update
+
     self.resource = resource_class.accept_invitation!(update_resource_params)
 
     inviter = User.find(resource.invited_by_id)
     movement = Movement.find(inviter.movements.first.id)
-    
+
     if resource.errors.empty?
       flash_message = resource.active_for_authentication? ? :updated : :updated_not_active                                                                                        
       set_flash_message :notice, flash_message
       sign_in(resource_name, resource)
-      respond_with resource, :location => after_accept_path_for(resource)
+      Ownership.create(user: resource, movement: movement)
+      redirect_to dashboard_movement_path(movement)
     else
       respond_with_navigational(resource){ render :edit }
     end
