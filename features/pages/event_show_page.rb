1b8e3166f916c81156c9ce5ef8a0a378a5f123ae
ParkDan <dpinch86@gmail.com>
2014-04-22 14:04:28 -0500

    minor changes to UI

--- features/pages/event_show_page.rb
+++ features/pages/event_show_page.rb
@@ -5,10 +5,10 @@ include Capybara::DSL
 	def delete_event (user)
 		click_link_or_button user.name 
  		click_link_or_button 'Profile'
-  		click_link_or_button 'event_tab'
-  		click_link('Crazy Event')
-  		click_link('Edit Event')
-  		click_link('Remove event')	
+  	click_link_or_button 'event_tab'
+  	click_link('Crazy Event')
+  	click_link('Edit Event')
+  	click_link('Remove event')	
 	end
 
 	def navigate_to(name)
