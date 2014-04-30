eebe2059fb9420ac6138966ef86a1a57e4b7f0b2
acslocum <acslocum@thoughtworks.com>
2014-04-30 13:59:54 -0500

    improving zip code search tests, attempting to find geo near problems

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -62,33 +62,50 @@ describe Event do
       end
     end
 
-    context "with published and unpublished movement" do
+    context "with a movement" do
       before do
         @test_zip = "60647"
-        @test_latitude = 10
-        @test_longitude = 50
+        @test_latitude = 41.9209
+        @test_longitude = -87.7043
         zip = FactoryGirl.create(:zipcode, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude)
+        
+        FactoryGirl.create(:zipcode, zip: "90210", latitude: 34.0901, longitude: -118.4065)
       end
 
-      it "finds a nearby event if assosiated movement is published" do
-        event = FactoryGirl.create(:approved_event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: published_movement)
-        expect(Event.near_zip(@test_zip, 50)).to eq([event])
+      context "with a published event" do
+        it "finds a nearby event in the same zip code if assosiated movement is published" do
+          event = FactoryGirl.create(:approved_event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: published_movement)
+          expect(Event.near_zip(@test_zip, 50)).to eq([event])
+        end
+
+        it "doesn't find a far away event even if assosiated movement is published" do
+          event = FactoryGirl.create(:approved_event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: published_movement)
+          expect(Event.near_zip("90210", 50)).to eq([])
+        end
+
+        it "finds several nearby events if assosiated movement is published" do
+           zips = [["60209",42.0497,-87.6794], ["60208",42.0586,-87.6845], ["60301",41.8886,-87.7986]]
+           @events = []
+           zips.each do |zip_tuple|
+             FactoryGirl.create(:zipcode, zip: zip_tuple[0], latitude: zip_tuple[1], longitude: zip_tuple[2])
+             @events << FactoryGirl.create(:approved_event, zip: zip_tuple[0], latitude: zip_tuple[1], longitude: zip_tuple[2], movement: published_movement)
+           end
+           events_near_zip = Event.near_zip(@test_zip, 50).to_a.sort_by(&:id)
+           expected_events = @events.sort_by(&:id)
+           expect(events_near_zip).to eql(expected_events)
+        end
       end
 
-      it "doesn't find a nearby event if assosiated movement is not published" do
-        event = FactoryGirl.create(:event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: unpublished_movement)
-        expect(Event.near_zip(@test_zip, 50)).to_not eql([event])
-      end
+      context "with an unpublished event" do
+        it "doesn't find a nearby event if assosiated movement is not published" do
+          event = FactoryGirl.create(:event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: unpublished_movement)
+          expect(Event.near_zip(@test_zip, 50)).to_not eql([event])
+        end
 
-      it "finds several nearby events if assosiated movement is published" do
-         zips = ["60247", "60250", "60240"]
-         @events = []
-         zips.each do |zip|
-           @events << FactoryGirl.create(:approved_event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: published_movement)
-         end
-         events_near_zip = Event.near_zip(@test_zip, 50).to_a.sort_by(&:id)
-         expected_events = @events.sort_by(&:id)
-         expect(events_near_zip).to eql(expected_events)
+        it "doesn't find a far away event if assosiated movement is not published" do
+          event = FactoryGirl.create(:event, zip: @test_zip, latitude: @test_latitude, longitude: @test_longitude, movement: unpublished_movement)
+          expect(Event.near_zip("90210", 50)).to_not eql([event])
+        end
       end
     end
   end
