c2e53ff936822be1c5897b53ae7058d471ea0bd9
ParkDan <dpinch86@gmail.com>
2014-05-01 19:01:40 -0500

    reverted config spike

--- config/application.rb
+++ config/application.rb
@@ -21,20 +21,5 @@ module Ffstrike
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
-    config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
   end
 end
