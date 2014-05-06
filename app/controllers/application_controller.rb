5422fd9e08075acfc6c8ad329f0b8c06ef4a9af7
YangaMGMT <YangaMGMT@gmail.com>
2014-05-06 17:27:37 -0500

    [Ayanga] Fixed Defect card #222: Redirects User to current page after Sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
 
   def after_sign_in_path_for(resource)
     if current_user.movements.empty?
-      root_path
+      session[:previous_url] || root_path
     else
       user_movements_path
     end
   end
 
-
-
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:invite).concat [:movement]
