7264660d636aab22dc0ef1a0991111313bbc09b3
Svetlana Ivanova <sivanova@thoughtworks.com>
2014-05-09 14:33:28 -0500

    fix cucumber test and update config files to include helpers on qa & test env

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -16,5 +16,6 @@ Ffstrike::Application.configure do
   config.action_mailer.default_url_options = { :host => "http://rally-qa.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_controller.include_all_helpers = true
 
 end
