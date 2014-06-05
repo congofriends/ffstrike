3993d62297cd060bc383c408ac3eb6e93442f1a9
ParkDan <dpinch86@gmail.com>
2014-06-05 13:59:56 -0500

    fix edit user profile defect, add js validations

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,29 @@
+function initializeProfileValidations() {
+  $('#edit-user-details').validate({
+    rules: {
+      'user[name]': { required: true },
+      'user[email]': { required: true },
+      'user[current_password]': { required: true }
+    },
+    messages: {
+      'user[name]': "Name is required",
+      'user[email]': "Email is required",
+      'user[current_password]': 'Current Password is required'
+    },
+    errorElement: "div",
+    errorClass: "text-danger",
+    errorId: "event_error",
+    errorPlacement: function (error, element) {
+      error.insertAfter(element);
+      element.closest('div').addClass('has-error');
+    },
+    success: function (label) {
+      label.closest('.form-group').removeClass('has-error').addClass('has-success');
+    }
+  });
+};
+
+$(document).ready(function() {
+  initializeProfileValidations();
+
+});
