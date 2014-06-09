00c446a3de542fc284920062052028b53219116c
ParkDan <dpinch86@gmail.com>
2014-06-09 10:04:16 -0500

    made dropdown arrow properly orient

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -115,6 +115,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
+    binding.pry
     params[:event][:event_type_id] = EventType.find_by(name: params[:event][:event_type]).id if params[:event][:event_type]
     params.require(:event).permit(:event_type_id, :address2, :name, :address, :location_details, :description, :city, :zip, :state, :start_time, :image, :end_time, :host_id, :notes, :forum_option, :approved)
   end
