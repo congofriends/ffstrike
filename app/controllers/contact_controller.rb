adf054c0db96bc67686e08a9bcb15dc4fa73a4e5
Tim Minkov <timothyminkov@gmail.com>
2014-06-10 15:31:16 -0700

    Fix spacing on contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -15,19 +15,19 @@ class ContactController < ApplicationController
     end
   end
 
-    def new_movement_msg
-        @movement = Movement.find_by_param(params[:id])
-        @message = Message.new
-    end
+  def new_movement_msg
+    @movement = Movement.find_by_param(params[:id])
+    @message = Message.new
+  end
 
-    def create_movement_msg
-        @message = Message.new(params[:message])
-        @movement =  Movement.find @message.host_id
+  def create_movement_msg
+    @message = Message.new(params[:message])
+    @movement =  Movement.find @message.host_id
 
-        if @message.valid?
-          ContactMailer.new_mvmt_message(@message).deliver
-          redirect_to(root_path, :notice => "Message was successfully sent.")
-        end
+    if @message.valid?
+      ContactMailer.new_mvmt_message(@message).deliver
+      redirect_to(root_path, :notice => "Message was successfully sent.")
     end
+  end
 
 end
