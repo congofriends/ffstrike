c151db25d64d72681567fa8f8147e56172ebec40
Tim Minkov <timothyminkov@gmail.com>
2014-06-10 15:37:34 -0700

    Memoize message in contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,12 +1,11 @@
 class ContactController < ApplicationController
+  before_filter :load_message
 
   def new_event_msg
     @event = Event.find_by_param(params[:id])
-    @message = Message.new
   end
 
   def create_event_msg
-    @message = Message.new(params[:message])
     @event =  Event.find @message.host_id
 
     if @message.valid?
@@ -17,17 +16,26 @@ class ContactController < ApplicationController
 
   def new_movement_msg
     @movement = Movement.find_by_param(params[:id])
-    @message = Message.new
   end
 
   def create_movement_msg
-    @message = Message.new(params[:message])
     @movement =  Movement.find @message.host_id
 
     if @message.valid?
       ContactMailer.new_mvmt_message(@message).deliver
       redirect_to(root_path, :notice => "Message was successfully sent.")
     end
+  end
+
+  private
+
+  def load_message
+    @message ||=
+      if params[:message]
+        Message.new(params[:message])
+      else
+        Message.new
+      end
   end
 
 end
