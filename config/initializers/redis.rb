a48e5c8858f35c9f3c1c4be0e9aaad523ec2435e
ParkDan <dpinch86@gmail.com>
2014-07-03 11:50:28 -0500

    added conditional to redis initializer for env vars

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+if ENV["REDISTOGO_URL"]
+	uri = URI.parse(ENV["REDISTOGO_URL"])
+	REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+end
