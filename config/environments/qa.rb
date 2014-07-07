3a50ecb6fe0939aea56dfd60789c3f6f2230d571
ParkDan <dpinch86@gmail.com>
2014-07-07 17:06:56 -0500

    added host table

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -8,7 +8,7 @@ Ffstrike::Application.configure do
     :user_name => "postmaster@ffstrike.mailgun.org",
     :password => "53bdbr1f8280"
   }
-  config.action_mailer.default_url_options = { :host => "http://shift-engage-qa.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "http://shift-engage-staging.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
