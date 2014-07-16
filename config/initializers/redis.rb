40151cc1efe25630690f920d25425140e3e67107
ParkDan <dpinch86@gmail.com>
2014-07-16 15:46:52 -0500

    sidekiq config check

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@
 #   REDIS = Redis.new(host: 'localhost', port: 6379)
 # elsif Rails.env.production?
 
-if ENV["REDISTOGO_URL"] && Rails.env.production?
+if ENV["REDISTOGO_URL"]
 	uri = URI.parse(ENV["REDISTOGO_URL"])
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
