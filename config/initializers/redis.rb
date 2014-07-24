089f9d112991f2008165177d4350062f07ff6906
ParkDan <dpinch86@gmail.com>
2014-07-24 17:39:05 -0500

    removed commented files

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,3 @@
-# if Rails.env.development?
-#   REDIS = Redis.new(host: 'localhost', port: 6379)
-# elsif Rails.env.production?
-
 if ENV["REDISTOGO_URL"]
 	uri = URI.parse(ENV["REDISTOGO_URL"])
 	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
