9de669a350b41b1ea9437cd7a3ba7d2353679319
YangaMGMT <YangaMGMT@gmail.com>
2014-08-15 15:48:08 -0500

    [Ayanga] removed unused fields from Movement creation

--- features/pages/create_movement_page.rb
+++ features/pages/create_movement_page.rb
@@ -4,8 +4,6 @@ include Capybara::DSL
 		visit "/movements/new"
 		fill_in 'movement_name', with: 'Congo Week'
 		fill_in 'movement_tagline', with: 'Move yo cat!'
-		fill_in 'movement_call_to_action', with: 'Call yo cat'
-		fill_in 'movement_extended_description', with: 'extend yo cat'
 		fill_in 'movement_video', with: 'https://www.youtube.com/watch?v=Kdgt1ZHkvnM'
 		click_link_or_button 'create_movement'
 	end
@@ -13,8 +11,6 @@ include Capybara::DSL
 	def fill_form
 		fill_in 'movement_name', with: 'Cats Move'
 		fill_in 'movement_tagline', with: 'Move yo cat!'
-		fill_in 'movement_call_to_action', with: 'Call yo cat'
-		fill_in 'movement_extended_description', with: 'extend yo cat'
 		fill_in 'movement_video', with: 'https://www.youtube.com/watch?v=Kdgt1ZHkvnM'
 	end
 
