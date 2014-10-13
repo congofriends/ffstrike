f85cac7997013641e14d7213cde351dd13515fe7
tmorris <tmorris@thoughtworks.com>
2014-10-13 16:54:33 -0500

    Do more work

---
+++ app/controllers/event_types_controller.rb
@@ -0,0 +1,6 @@
+class EventTypesController < ApplicationController
+
+	def load_images
+		redirect_to root_path
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007112816) do
+ActiveRecord::Schema.define(version: 20141013184632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,11 @@ ActiveRecord::Schema.define(version: 20141007112816) do
   add_index "attendances", ["user_id"], name: "index_attendances_on_user_id", using: :btree
 
   create_table "event_types", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "events", force: true do |t|
