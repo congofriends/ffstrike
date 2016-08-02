1b324635456e8b88c062066e9be2e68ab0cdea30
Andy Slocum <acslocum@thoughtworks.com>
2016-08-02 11:55:18 -0400

    trying to get tests to pass

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,3 +47,10 @@ Ffstrike::Application.configure do
   config.active_support.deprecation = :stderr
 
 end
+
+Recaptcha.configure do |config|
+  config.public_key  = '6Ld3aiQTAAAAAE2KdNaOAqRl_F0HQTyHL4y3qAAh'
+  config.private_key = 'secret'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
