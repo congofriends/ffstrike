80ba015e35a3e8e67f2d9a4aef12970bc2f896d5
Andy Slocum <acslocum@thoughtworks.com>
2016-08-02 11:50:04 -0400

    removing hard coded recaptcha config

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld3aiQTAAAAAE2KdNaOAqRl_F0HQTyHL4y3qAAh'
-  config.private_key = 'secret'
+  #config.public_key  = '6Ld3aiQTAAAAAE2KdNaOAqRl_F0HQTyHL4y3qAAh'
+  #config.private_key = 'secret'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
